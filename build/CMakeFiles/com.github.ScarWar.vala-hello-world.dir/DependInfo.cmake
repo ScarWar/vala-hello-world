841a4ad6b531bb41ba0aaae3a283a61c3b9f8b94
Ariel Bereslavsky <ariael2009@gmail.com>
2017-12-29 23:27:12 +0200

    Fix(Project structure): Clean project files

---
+++ build/CMakeFiles/com.github.ScarWar.vala-hello-world.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ariel/Projects/elementaryOS/hello-world/hello-again/build/src/hello-again.c" "/home/ariel/Projects/elementaryOS/hello-world/hello-again/build/CMakeFiles/com.github.ScarWar.vala-hello-world.dir/src/hello-again.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GETTEXT_PACKAGE=\"com.github.ScarWar.vala-hello-world\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
